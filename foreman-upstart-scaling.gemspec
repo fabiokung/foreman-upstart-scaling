8fae8666b883f81402523e4963e66698fd8fa5af
Fabio Kung <fabio.kung@gmail.com>
2012-06-04 18:27:21 -0700

    first version

---
+++ foreman-upstart-scaling.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = "foreman-upstart-scaling"
+  s.email = "fabio.kung@gmail.com"
+  s.version = "0.1"
+  s.description = "Upstart exporter for foreman, that supports worker scaling via env vars."
+  s.summary = "Upstart configuration generator for foreman"
+  s.authors = ["Fabio Kung"]
+  s.homepage = "http://github.com/fabiokung/foreman-upstart-scaling"
+
+  s.files = Dir["lib/**/*.rb"] + Dir["data/**/*.erb"]
+  s.require_paths = ["lib"]
+  s.add_dependency "foreman", ">= 0.46.0"
+end
+
---
+++ lib/foreman/export/upstart_scaling.rb
@@ -0,0 +1,41 @@
+require "erb"
+require "fileutils"
+require "foreman/export"
+
+class Foreman::Export::UpstartScaling < Foreman::Export::Base
+
+  def export
+    error("Must specify a location") unless location
+    FileUtils.mkdir_p location
+    @location = Pathname.new(@location)
+
+    app = self.app || File.basename(engine.directory)
+    user = self.user || app
+    template_root = self.template
+
+    Dir["#{location}/#{app}*.conf"].each do |file|
+      say "cleaning up: #{file}"
+      FileUtils.rm(file)
+    end
+
+    master_template = export_template("upstart_scaling", "master.conf.erb", template_root)
+    master_config   = ERB.new(master_template).result(binding)
+    write_file "#{location}/#{app}.conf", master_config
+
+    engine.procfile.entries.each do |process|
+      process_master_template = export_template("upstart_scaling", "process_master.conf.erb", template_root)
+      process_master_config = ERB.new(process_master_template).result(binding)
+      write_file "#{location}/#{app}-#{process.name}.conf", process_master_config
+
+      process_instances_template = export_template("upstart_scaling", "process_instances.conf.erb", template_root)
+      process_instances_config = ERB.new(process_instances_template).result(binding)
+      write_file "#{location}/#{app}-#{process.name}-instances.conf", process_instances_config
+
+      process_template = export_template("upstart_scaling", "process.conf.erb", template_root)
+      process_config = ERB.new(process_template).result(binding)
+      write_file "#{location}/#{app}-#{process.name}-instance.conf", process_config
+    end
+  end
+
+end
+
